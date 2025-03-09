98badff7736b6d464de1d895213c2f04018cdc50
Antonia Statt <astatt@github.com>
2025-03-09 13:47:01 -0500

    Added missing cu file to CmakeList

--- hoomd/md/ComputeThermoSLLODGPU.cc
+++ hoomd/md/ComputeThermoSLLODGPU.cc
@@ -1,13 +1,14 @@
 // Copyright (c) 2009-2025 The Regents of the University of Michigan.
 // Part of HOOMD-blue, released under the BSD 3-Clause License.
 
-/*! \file ComputeThermoGPU.cc
-    \brief Contains code for the ComputeThermoGPU class
+/*! \file ComputeThermoSLLODGPU.cc
+    \brief Contains code for the ComputeThermoSLLODGPU class
  */
 
 #include "ComputeThermoSLLODGPU.h"
 #include "ComputeThermoSLLODGPU.cuh"
 #include "ComputeThermoGPU.cuh"
+
 #ifdef ENABLE_MPI
 #include "hoomd/Communicator.h"
 #include "hoomd/HOOMDMPI.h"
--- hoomd/md/ComputeThermoSLLODGPU.cu
+++ hoomd/md/ComputeThermoSLLODGPU.cu
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2025 The Regents of the University of Michigan.
 // Part of HOOMD-blue, released under the BSD 3-Clause License.
 
+#include "ComputeThermoGPU.cuh"
 #include "ComputeThermoSLLODGPU.cuh"
 #include "hoomd/HOOMDMath.h"
 #include "hoomd/VectorMath.h"
