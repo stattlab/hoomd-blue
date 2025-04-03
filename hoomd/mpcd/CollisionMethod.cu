378b167c6b882473487d000d580249ccd5127d28
mzbush <mzb0108@auburn.edu>
2025-04-03 13:21:15 -0500

    Implement rigid collision driver functions

--- hoomd/mpcd/CollisionMethod.cu
+++ hoomd/mpcd/CollisionMethod.cu
@@ -22,6 +22,10 @@ __global__ void store_initial_embedded_group_velocities(Scalar4 d_initial_velo,
                                                         const unsigned int* d_embed_group,
                                                         const unsigned int N)
     {
+    // one thread per particle
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= N_tot)
+        return;
     }
 
 __global__ void accumulate_rigid_body_momenta(Scalar3 d_linmom_accum,
@@ -36,6 +40,10 @@ __global__ void accumulate_rigid_body_momenta(Scalar3 d_linmom_accum,
                                               const BoxDim global_box,
                                               const uint64_t timestep)
     {
+    // one thread per particle
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= N_tot)
+        return;
     }
 
 __global__ void transfer_rigid_body_momenta(Scalar3 d_linmom_accum,
@@ -48,9 +56,105 @@ __global__ void transfer_rigid_body_momenta(Scalar3 d_linmom_accum,
                                             const unsigned int* d_rtag,
                                             const uint64_t timestep)
     {
+    // one thread per particle
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= N_tot)
+        return;
     }
 
     } // end namespace kernel
+
+cudaError_t store_initial_embedded_group_velocities(Scalar4* d_initial_velo,
+                                                    const Scalar4* d_vel_embed,
+                                                    const unsigned int* d_embed_group,
+                                                    const unsigned int num_group,
+                                                    const unsigned int block_size)
+    {
+    unsigned int max_block_size;
+    cudaFuncAttributes attr;
+    cudaFuncGetAttributes(&attr,
+                          (const void*)mpcd::gpu::kernel::store_initial_embedded_group_velocities);
+    max_block_size = attr.maxThreadsPerBlock;
+
+    unsigned int run_block_size = min(block_size, max_block_size);
+
+    dim3 grid(num_group / run_block_size + 1);
+    mpcd::gpu::kernel::store_initial_embedded_group_velocities<<<grid, run_block_size>>>(
+        d_initial_velo,
+        d_vel_embed,
+        d_embed_group,
+        num_group);
+
+    return cudaSuccess;
+    }
+
+cudaError_t accumulate_rigid_body_momenta(Scalar3* d_linmom_accum,
+                                          Scalar3* d_angmom_accum,
+                                          const Scalar4* d_initial_velo,
+                                          const unsigned int* d_embed_group,
+                                          const Scalar4* d_postype,
+                                          const Scalar4* d_velocity,
+                                          const int3* d_image,
+                                          const unsigned int* d_body,
+                                          const unsigned int* d_rtag,
+                                          const BoxDim global_box,
+                                          const unsigned int num_group,
+                                          const unsigned int block_size)
+    {
+    unsigned int max_block_size;
+    cudaFuncAttributes attr;
+    cudaFuncGetAttributes(&attr, (const void*)mpcd::gpu::kernel::accumulate_rigid_body_momenta);
+    max_block_size = attr.maxThreadsPerBlock;
+
+    unsigned int run_block_size = min(block_size, max_block_size);
+
+    dim3 grid(num_group / run_block_size + 1);
+    mpcd::gpu::kernel::accumulate_rigid_body_momenta<<<grid, run_block_size>>>(d_linmom_accum,
+                                                                               d_angmom_accum,
+                                                                               d_initial_velo,
+                                                                               d_embed_group,
+                                                                               d_postype,
+                                                                               d_velocity,
+                                                                               d_image,
+                                                                               d_body,
+                                                                               d_rtag,
+                                                                               global_box,
+                                                                               num_group);
+
+    return cudaSuccess;
+    }
+
+cudaError_t transfer_rigid_body_momenta(Scalar3* d_linmom_accum,
+                                        Scalar3* d_angmom_accum,
+                                        Scalar4* d_velocity,
+                                        const Scalar4* d_orientation,
+                                        Scalar4* d_angmom,
+                                        const Scalar3* d_inertia,
+                                        const unsigned int* d_body,
+                                        const unsigned int* d_rtag,
+                                        const unsigned int num_total,
+                                        const unsigned int block_size)
+    {
+    unsigned int max_block_size;
+    cudaFuncAttributes attr;
+    cudaFuncGetAttributes(&attr, (const void*)mpcd::gpu::kernel::transfer_rigid_body_momenta);
+    max_block_size = attr.maxThreadsPerBlock;
+
+    unsigned int run_block_size = min(block_size, max_block_size);
+
+    dim3 grid(num_total / run_block_size + 1);
+    mpcd::gpu::kernel::transfer_rigid_body_momenta<<<grid, run_block_size>>>(d_linmom_accum,
+                                                                             d_angmom_accum,
+                                                                             d_velocity,
+                                                                             d_orientation,
+                                                                             d_angmom,
+                                                                             d_inertia,
+                                                                             d_body,
+                                                                             d_rtag,
+                                                                             num_total);
+
+    return cudaSuccess;
+    }
     } // end namespace gpu
     } // end namespace mpcd
     } // end namespace hoomd
--- hoomd/mpcd/CollisionMethod.cuh
+++ hoomd/mpcd/CollisionMethod.cuh
@@ -17,33 +17,35 @@ namespace mpcd
 namespace gpu
     {
 
-cudaError_t store_initial_embedded_group_velocities(Scalar4 d_initial_velo,
+cudaError_t store_initial_embedded_group_velocities(Scalar4* d_initial_velo,
                                                     const Scalar4* d_vel_embed,
                                                     const unsigned int* d_embed_group,
                                                     const unsigned int num_group,
                                                     const unsigned int block_size);
 
-void accumulate_rigid_body_momenta(Scalar3 d_linmom_accum,
-                                   Scalar3 d_angmom_accum,
-                                   const Scalar4 d_initial_velo,
-                                   const unsigned int* d_embed_group,
-                                   const Scalar4* d_postype,
-                                   const Scalar4* d_velocity,
-                                   const int3* d_image,
-                                   const unsigned int* d_body,
-                                   const unsigned int* d_rtag,
-                                   const BoxDim global_box,
-                                   const uint64_t timestep);
+cudaError_t accumulate_rigid_body_momenta(Scalar3* d_linmom_accum,
+                                          Scalar3* d_angmom_accum,
+                                          const Scalar4* d_initial_velo,
+                                          const unsigned int* d_embed_group,
+                                          const Scalar4* d_postype,
+                                          const Scalar4* d_velocity,
+                                          const int3* d_image,
+                                          const unsigned int* d_body,
+                                          const unsigned int* d_rtag,
+                                          const BoxDim global_box,
+                                          const unsigned int num_group,
+                                          const unsigned int block_size);
 
-void transfer_rigid_body_momenta(Scalar3 d_linmom_accum,
-                                 Scalar3 d_angmom_accum,
-                                 Scalar4* d_velocity,
-                                 const Scalar4* d_orientation,
-                                 Scalar4* d_angmom,
-                                 const Scalar3* d_inertia,
-                                 const unsigned int* d_body,
-                                 const unsigned int* d_rtag,
-                                 const uint64_t timestep);
+cudaError_t transfer_rigid_body_momenta(Scalar3* d_linmom_accum,
+                                        Scalar3* d_angmom_accum,
+                                        Scalar4* d_velocity,
+                                        const Scalar4* d_orientation,
+                                        Scalar4* d_angmom,
+                                        const Scalar3* d_inertia,
+                                        const unsigned int* d_body,
+                                        const unsigned int* d_rtag,
+                                        const unsigned int num_total,
+                                        const unsigned int block_size);
 
     } // end namespace gpu
     } // end namespace mpcd
