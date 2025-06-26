d773a580ac03971c9aa2bd97d7a3d729d0ece1f3
Joshua A. Anderson <joaander@umich.edu>
2025-06-26 08:13:24 -0400

    Bump version to 5.3.0.

--- sphinx-doc/conf.py
+++ sphinx-doc/conf.py
@@ -85,8 +85,8 @@ year = datetime.date.today().year
 copyright = f"2009-{year} The Regents of the University of Michigan"
 author = "The Regents of the University of Michigan"
 
-version = "5.2.0"
-release = "5.2.0"
+version = "5.3.0"
+release = "5.3.0"
 
 language = "en"
 
